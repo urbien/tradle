d5d2629db913ba390578be82136100267d7923a6
Mark Vayngrib <mark.vayngrib@lablz.com>
2014-11-10 23:35:03 -0800

    defer to installation scripts in projects

--- install.sh
+++ install.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-mkdir -p bitjoe
-mkdir -p bitjoe/conf
-wget https://github.com/urbien/bitjoe/raw/master/build/bitjoe.jar -P bitjoe
-wget https://github.com/urbien/bitjoe/raw/master/conf/config.json -P bitjoe/conf
-
-mkdir -p bitkeeper
-mkdir -p bitkeeper/conf
-wget https://github.com/urbien/bitkeeper/raw/master/build/bitkeeper.jar -P bitkeeper
-wget https://github.com/urbien/bitkeeper/raw/master/conf/config.json -P bitkeeper/conf
+wget -O - https://github.com/urbien/bitjoe/raw/master/install.sh | bash
+wget -O - https://github.com/urbien/bitkeeper/raw/master/install.sh | bash
